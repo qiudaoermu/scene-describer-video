d78aefa963a7d030b437dcd27e36557c594a6cf9
陈振飞 <qiudaoermu@gmail.com>
2025-09-12 16:43:06 +0800

    remove redundancy

--- app/getPexelsVideo.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'pexels';
-
-const client = createClient(
-  "oVaOZUHlLhFNilLPJQ1jvAPS8BFngRiKDDsEvG43ivJBXJF5VfuBqAG6"
-);
-
-client.videos.popular({ per_page: 1,page:2 }).then(videos => {
-      console.log(videos,'videos')
-});
